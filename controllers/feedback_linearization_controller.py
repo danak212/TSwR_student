c47b1572519dae25d4161602d8957d5ea660ce25
danak212 <danak112@wp.pl>
2024-06-12 13:06:25 +0200

    miniTask_No_1

--- controllers/feedback_linearization_controller.py
+++ controllers/feedback_linearization_controller.py
@@ -12,4 +12,11 @@ class FeedbackLinearizationController(Controller):
         Please implement the feedback linearization using self.model (which you have to implement also),
         robot state x and desired control v.
         """
-        return NotImplementedError()
+        q1, q2, q1_dot, q2_dot = x
+
+        kp=1.0
+        kd=1.0
+        v = q_r_ddot + kd * ([q1_dot, q2_dot] - q_r_dot) + kp * ([q1, q2] - q_r)
+        t = self.model.M(x) @ v + self.model.C(x) @ [q1_dot, q2_dot]
+        
+        return t
